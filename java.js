31f36dbfe27420074d273523e0e5265bff445955
trosario102 <t.rosario102@gmail.com>
2022-02-17 13:32:05 -0600

    made and centered initial grid

---
+++ java.js
@@ -0,0 +1,30 @@
+let container = document.getElementById('container');
+let rows = document.getElementsByClassName("gridRow");
+let cells = document.getElementsByClassName("cell");
+
+function makeRows(num) {
+
+
+
+    for (let i = 0; i < num; i++) {
+        let row = document.createElement('div');
+        container.appendChild(row).className = 'gridRow';
+
+    }
+
+}
+
+
+function makeColumns(num) {
+
+    for (let i = 0; i < rows.length; i++) {
+        for (let j = 0; j < num; j++) {
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = 'cell';
+        }
+    }
+
+}
+
+makeRows(16);
+makeColumns(16);
